5203ea2a11675271d1ffc804f7ad9cb37d809310
DerekChristy <kered.christy@gmail.com>
2020-05-11 12:19:06 +0530

    added get post user

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,8 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
 
 router.get('/', (req, res) => {
   res.send('Hi from cv-19 API');
+});
+// TODO: complete user schema
+router.get('/users', (req, res) => {
+  User.find((err, users) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.status(200).json(users);
+    }
+  });
+});
+
+router.post('/user', (req, res) => {
+  const data = req.body;
+  const user = new User(data);
+  user.save((err, newUser) => {
+    if (err) {
+      console.log(err);
+      res.status(401).json(err);
+    } else {
+      res.status(200).json(newUser);
+    }
+  });
 });
 
 module.exports = router;
